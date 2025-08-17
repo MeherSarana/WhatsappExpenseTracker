38b89f32c734b05b78bcd3485997df954541f2ea
MeherSarana <mehersarana@gmail.com>
2025-08-17 11:21:01 +0530

    Second Commit

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI, Form
 from fastapi.responses import PlainTextResponse
+from twilio.rest import Client
+import os
+
+account_sid = os.getenv("account_sid")
+auth_token = os.getenv("auth_token")
+client = Client(account_sid, auth_token)
 
 app = FastAPI()
 
 @app.post("/whatsapp")
 async def whatsapp_webhook(Body: str = Form(...), From: str = Form(...)):
     print(f"Message from {From}: {Body}")
+
+    #reply to user
+    client.messages.create(
+    from_="whatsapp:+14155238886",
+    to="whatsapp:+918125056526",
+    body=f"Received the message: {Body}"
+    )
+
     return PlainTextResponse("OK")
+
